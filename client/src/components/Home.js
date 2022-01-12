6d0bf882050dfd51407b08e6e7cf94f5661e52aa
Seemanto <seemanto@hotmail.com>
2022-01-12 17:33:47 +0600

    minor fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,72 +1,73 @@
 import {
-    Container,
-    Button,
-    Input,
-    InputGroup,
-    InputRightElement,
+  Container,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 
 function Home() {
-    const history = useHistory();
+  const history = useHistory();
 
-    return (
-        <div>
-            <Container maxW="container.sm">
-                <InputGroup size="lg" marginTop="100px">
-                    <Input pr="4.5rem" type="text" placeholder="Search User" />
-                    <InputRightElement width="5rem">
-                        <Button h="1.75rem" size="sm">
-                            Search
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
+  return (
+    <div>
+      <Container maxW="container.sm">
+        <InputGroup size="lg" marginTop="100px">
+          <Input pr="4.5rem" type="text" placeholder="Search User" />
+          <InputRightElement width="5rem">
+            <Button h="1.75rem" size="sm">
+              Search
+            </Button>
+          </InputRightElement>
+        </InputGroup>
 
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        alignContent: "center",
-                        justifyContent: "center",
-                        justifyItems: "center",
-                        padding: "10px",
-                        marginTop: "50px",
-                    }}
-                >
-                    <div style={{ margin: "20px" }}>
-                        <Button
-                            colorScheme="blue"
-                            size="lg"
-                            onClick={() => history.push("/add-single-user")}
-                        >
-                            Add Single User
-                        </Button>
-                    </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            alignContent: "center",
+            justifyContent: "center",
+            justifyItems: "center",
+            padding: "10px",
+            marginTop: "50px",
+            flexWrap: "wrap",
+          }}
+        >
+          <div style={{ margin: "20px" }}>
+            <Button
+              colorScheme="blue"
+              size="md"
+              onClick={() => history.push("/add-single-user")}
+            >
+              Add Single User
+            </Button>
+          </div>
 
-                    <div style={{ margin: "20px" }}>
-                        <Button
-                            colorScheme="blue"
-                            size="lg"
-                            onClick={() => history.push("/add-multiple-users")}
-                        >
-                            Add Multiple Users
-                        </Button>
-                    </div>
+          <div style={{ margin: "20px" }}>
+            <Button
+              colorScheme="blue"
+              size="md"
+              onClick={() => history.push("/add-multiple-users")}
+            >
+              Add Multiple Users
+            </Button>
+          </div>
 
-                    <div style={{ margin: "20px" }}>
-                        <Button
-                            colorScheme="blue"
-                            size="lg"
-                            onClick={() => history.push("/view-user")}
-                        >
-                            View All Users
-                        </Button>
-                    </div>
-                </div>
-            </Container>
+          <div style={{ margin: "20px" }}>
+            <Button
+              colorScheme="blue"
+              size="md"
+              onClick={() => history.push("/view-user")}
+            >
+              View All Users
+            </Button>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/components/Multiple.js
+++ client/src/components/Multiple.js
@@ -4,64 +4,64 @@ import { FileUploader } from "react-drag-drop-files";
 import axios from "axios";
 
 function Multiple() {
-    const [file, setFile] = useState(null);
-    const handleChange = (file) => {
-        setFile(file);
-    };
-    const fileTypes = ["CSV"];
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+  const fileTypes = ["CSV"];
 
-    const handelSubmit = () => {
-        console.log(file);
+  const handelSubmit = () => {
+    console.log(file);
 
-        const formData = new FormData();
-        formData.append("uploadfile", file);
+    const formData = new FormData();
+    formData.append("uploadfile", file);
 
-        axios({
-            method: "POST",
-            url: "http://localhost:8080/api/user/uploadfile",
-            data: formData,
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
-        })
-            .then((response) => {
-                if (response.status === 200) {
-                    alert("Success, firm added");
-                } else {
-                    alert("Error occurred");
-                }
+    axios({
+      method: "POST",
+      url: "http://localhost:8080/api/user/uploadfile",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("Success");
+        } else {
+          alert("Error");
+        }
 
-                setFile(null);
-            })
-            .catch((e) => {
-                console.log(e);
+        setFile(null);
+      })
+      .catch((e) => {
+        console.log(e);
 
-                setFile(null);
-            });
-    };
+        setFile(null);
+      });
+  };
 
-    return (
-        <div>
-            <Container centerContent>
-                <Stack spacing={3} marginTop="30px">
-                    <FileUploader
-                        handleChange={handleChange}
-                        name="file"
-                        types={fileTypes}
-                    />
+  return (
+    <div>
+      <Container centerContent>
+        <Stack spacing={3} marginTop="30px">
+          <FileUploader
+            handleChange={handleChange}
+            name="file"
+            types={fileTypes}
+          />
 
-                    <Button
-                        colorScheme="cyan"
-                        variant="solid"
-                        disabled={file === null ? true : false}
-                        onClick={handelSubmit}
-                    >
-                        Submit
-                    </Button>
-                </Stack>
-            </Container>
-        </div>
-    );
+          <Button
+            colorScheme="cyan"
+            variant="solid"
+            disabled={file === null ? true : false}
+            onClick={handelSubmit}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
 
 export default Multiple;
